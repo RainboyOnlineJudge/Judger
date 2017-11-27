e856b82902983e8998bda69ba5eb6a131efed3f4
rainboy <rainboylvx.@qq.com>
2017-11-27 21:11:56 +0800

    fix err

--- tests/Python_and_core/test.py
+++ tests/Python_and_core/test.py
@@ -7,7 +7,7 @@ from unittest import TestCase, main
 from testcase.integration.test import IntegrationTest
 from testcase.seccomp.test import SeccompTest
 
-ver = _judger.VERSION
-print("Judger version %d.%d.%d" % ((ver >> 16) & 0xff, (ver >> 8) & 0xff, ver & 0xff))
-print(sys.version)
+# ver = _judger.VERSION
+# print("Judger version %d.%d.%d" % ((ver >> 16) & 0xff, (ver >> 8) & 0xff, ver & 0xff))
+# print(sys.version)
 main()
