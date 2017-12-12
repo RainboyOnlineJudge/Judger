7623c51d627e8727c57f89f0e8925484f7bb792f
rainboy <rainboylvx.@qq.com>
2017-12-12 10:22:14 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 rm -rf build && mkdir build && cd build && cmake ..
 make || exit 1
 make install
-cd ../bindings/Python && rm -rf build
-cd ../bindings/python-rj/
+cd ../bindings/python-rj/ && rm -rf build
 python3 setup.py install || exit 1
 #cd ../../tests/Python_and_core && python3 test.py
